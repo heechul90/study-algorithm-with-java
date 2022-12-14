df6720774751fe80460d6306160ee92b3ed947f3
hclee <hclee@dataeum.kr>
2022-12-14 13:15:47 +0900

    add repository for algorithm with java

---
+++ src/test/coding/programmers/learn/level3/Test01.java
@@ -0,0 +1,11 @@
+package test.coding.programmers.learn.level3;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        //캠핑
+
+        int n = 0;
+        int[][] data;
+    }
+}
